c9909d4320482fecd57ed969d455c4e3e3789c40
Jonas Luebbers <jluebbers@squarespace.com>
2018-04-10 17:01:28 -0400

    Add path object to tile

--- src/tile.js
+++ src/tile.js
@@ -3,18 +3,20 @@ import { degToRad } from './util'
 export default class Tile {
     constructor(alt) {
         this.alt = alt
+
         this.a = alt ? degToRad(18) : degToRad(36)
         this.w = Math.cos(this.a)
         this.h = Math.sin(this.a)
+
+        this.path = new Path2D()
+        this.path.moveTo(this.w, 0)
+        this.path.lineTo(0, this.h)
+        this.path.lineTo(-this.w, 0)
+        this.path.lineTo(0, -this.h)
+        this.path.closePath()
     }
 
     draw(context) {
-        context.beginPath()
-        context.moveTo(this.w, 0)
-        context.lineTo(0, this.h)
-        context.lineTo(-this.w, 0)
-        context.lineTo(0, -this.h)
-        context.closePath()
-        context.stroke()
+        context.stroke(this.path)
     }
 }
