d1f3ea5ccce6d8b3c1cd2f6aecace7567d9f1552
Jonas Luebbers <jluebbers@squarespace.com>
2018-04-11 13:13:57 -0400

    Add tile neightbor

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import { Matrix3 } from 'three'
 import Tile from './tile'
 
 const canvas = document.getElementById('canvas')
@@ -15,16 +14,10 @@ context.translate(canvas.width / 2, canvas.height / 2)
 context.scale(scale, scale)
 
 const tilesInView = new Set()
-
-const leftTransform = new Matrix3()
-leftTransform.setUvTransform(-2, 0, 1, 1, 0, 0, 0)
-const leftTile = new Tile(leftTransform, false)
-tilesInView.add(leftTile)
-
-const rightTransform = new Matrix3()
-rightTransform.setUvTransform(2, 0, 1, 1, 0, 0, 0)
-const rightTile = new Tile(rightTransform, true)
-tilesInView.add(rightTile)
+const firstTile = new Tile()
+const secondTile = firstTile.createNeighbor()
+tilesInView.add(firstTile)
+tilesInView.add(secondTile)
 
 function draw() {
     requestAnimationFrame(draw)
--- src/tile.js
+++ src/tile.js
@@ -1,26 +1,44 @@
-import { degToRad, matrixToTransform } from './util'
-import { Matrix3 } from 'three'
+import { degToRad } from './util'
+import { Vector2 } from 'three'
+
+const alpha = degToRad(18)
+const beta = degToRad(36)
 
 export default class Tile {
-    constructor(transform, alt) {
-        this.transform = transform || new Matrix3()
-        this.alt = alt
+    constructor(type) {
+        this.type = type || false
+        this.position = new Vector2()
+        this.dimensions = new Vector2()
+        this.rotation = 0
 
-        this.a = alt ? degToRad(18) : degToRad(36)
-        this.w = Math.cos(this.a)
-        this.h = Math.sin(this.a)
+        this.internalAngle = type ? alpha : beta
+        this.dimensions.x = Math.cos(this.internalAngle)
+        this.dimensions.y = Math.sin(this.internalAngle)
 
         this.path = new Path2D()
-        this.path.moveTo(this.w, 0)
-        this.path.lineTo(0, this.h)
-        this.path.lineTo(-this.w, 0)
-        this.path.lineTo(0, -this.h)
+        this.path.moveTo(this.dimensions.x, 0)
+        this.path.lineTo(0, -this.dimensions.y)
+        this.path.lineTo(-this.dimensions.x, 0)
+        this.path.lineTo(0, this.dimensions.y)
         this.path.closePath()
+    }
+
+    get edgeTransform() {
+        const v = new Vector2(this.dimensions.x / 2, this.dimensions.y / 2)
+        return v.rotateAround(new Vector2(), this.rotation)
+    }
+
+    createNeighbor() {
+        const that = new Tile(true)
+        that.rotation = this.rotation + Math.PI - this.internalAngle + that.internalAngle
+        that.position = this.position.clone().add(this.edgeTransform).sub(that.edgeTransform)
+        return that
     }
 
     draw(context) {
         context.save()
-        context.transform(...matrixToTransform(this.transform))
+        context.translate(this.position.x, this.position.y)
+        context.rotate(this.rotation)
         context.stroke(this.path)
         context.restore()
     }
